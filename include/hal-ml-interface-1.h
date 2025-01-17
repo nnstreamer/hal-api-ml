b50ac84a8f0534cec88931b3f09b2a0becb8f19f
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2025-01-17 18:50:45 +0900

    Remove nns header
    - Remove two header files tensor_typedef.h and
      nnstreamer_plugin_api_filter.h
    - Change func_pointer signature to void and int type
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- include/hal-ml-interface-1.h
+++ include/hal-ml-interface-1.h
@@ -19,9 +19,6 @@
 #ifndef __HAL_ML_INTERFACE_1__
 #define __HAL_ML_INTERFACE_1__
 
-#include "tensor_typedef.h"
-#include "nnstreamer_plugin_api_filter.h"
-
 #include "hal-ml-types.h"
 
 #ifdef __cplusplus
@@ -44,17 +41,17 @@ typedef struct _hal_backend_ml_funcs {
   int (*deinit)(void *backend_private);
 
   /**< Configure ml framework */
-  int (*configure_instance)(void *backend_private, const GstTensorFilterProperties *prop);
+  int (*configure_instance)(void *backend_private, const void *prop);
   /**< Invoke */
-  int (*invoke)(void *backend_private, const GstTensorMemory *input, GstTensorMemory *output);
+  int (*invoke)(void *backend_private, const void *input, void *output);
   /**< Invoke dynamic */
-  int (*invoke_dynamic)(void *backend_private, GstTensorFilterProperties *prop, const GstTensorMemory *input, GstTensorMemory *output);
+  int (*invoke_dynamic)(void *backend_private, void *prop, const void *input, void *output);
   /**< Get framework info */
-  int (*get_framework_info)(void *backend_private, GstTensorFilterFrameworkInfo *framework_info);
+  int (*get_framework_info)(void *backend_private, void *framework_info);
   /**< Get model info */
-  int (*get_model_info)(void *backend_private, model_info_ops ops, GstTensorsInfo *in_info, GstTensorsInfo *out_info);
+  int (*get_model_info)(void *backend_private, int ops, void *in_info, void *out_info);
   /**< Handle event */
-  int (*event_handler)(void *backend_private, event_ops ops, GstTensorFilterFrameworkEventData *data);
+  int (*event_handler)(void *backend_private, int ops, void *data);
 } hal_backend_ml_funcs;
 
 /**
